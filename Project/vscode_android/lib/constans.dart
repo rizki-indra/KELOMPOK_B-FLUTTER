499c8ba6f18386db160f61875db3c525bebf32ae
firdaus975 <choirulahamad31@gmail.com>
2020-11-18 16:08:09 +0700

    Progres palete warna login

---
+++ Project/vscode_android/lib/constans.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const primaryColor = Color(0xE7E7DE);
+  static const primaryDarkColor = Color(0xff607Cbf);
+  static const underlineTextField = Color(0x00587A);
+  static const hintColor = Color(0xFFA7A7A7);
+  static const buttonColor = Color(0xFF00587A);
+  static const appBar = Color(0xFF7A96DD);
+}
