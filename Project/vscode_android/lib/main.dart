252fdbf99307d91e5d6957f5bdedf3ec7928b027
firdaus975 <choirulahamad31@gmail.com>
2021-01-13 17:51:01 +0700

    Update main.dart
    namabah route

--- Project/vscode_android/lib/main.dart
+++ Project/vscode_android/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:vscode_android/camera.dart';
+import 'package:vscode_android/datauser.dart';
 import 'package:vscode_android/loginView.dart';
 import 'package:vscode_android/home_screen.dart';
+import 'package:vscode_android/artikel.dart';
+import 'package:vscode_android/donasi.dart';
+import 'package:vscode_android/dataartikel.dart';
 
 import 'Registerview.dart';
 
@@ -20,6 +24,10 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
+        DataArtikel.routeName: (context) => DataArtikel(),
+        Donasi.routeName: (context) => Donasi(),
+        Artikel.routeName: (context) => Artikel(),
+        GetDataUser.routeName: (context) => GetDataUser(),
         HomeScreen.routeName: (context) => HomeScreen(),
         RegisterPage.routeName: (context) => RegisterPage(),
         CameraScreen.routeName: (context) => CameraScreen()
